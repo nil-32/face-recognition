60761da89c985d2fc0f24357f6f3010eba1760d2
nil <113453210+nil-32@users.noreply.github.com>
2024-07-26 11:03:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import concurrent.futures
 import datetime
-
+import tkinter as tk
+from tkinter import ttk
 import face_recognition
 import sys, os
 import cv2
 import numpy as np
+from PIL import Image, ImageTk
 
 class FaceRecognition:
 
@@ -28,13 +30,29 @@ class FaceRecognition:
 
             self.known_face_encodings.append(face_encoding)
             self.known_face_names.append(image)
-            self.data.update({image: {"name":image.split(".")[0],"last attendence time" : "non"}})
+            self.data.update({image: {"name":image.split(".")[0],"last attendance time" : "non"}})
         print(self.data)
         #print(self.known_face_names)
 
 
     def run_recognition(self):
+        root = tk.Tk()
+        root.geometry("1100x500")
+        label_widget = ttk.Label(root)
+        table = ttk.Treeview(root, columns=('name', 'last attendance'), show='headings')
 
+        table.column("# 1")
+        table.heading("# 1", text="name")
+        table.column("# 2", anchor=tk.NW)
+        table.heading("# 2", text="last attendance time")
+        root.columnconfigure(0,weight=1)
+        root.columnconfigure(1, weight=1)
+        for key in self.data.keys():
+            table.insert(parent='',text= "key",index= tk.END ,values= (self.data[key]['name'], self.data[key]['last attendance time']))
+        table.column("#0", width=0, stretch=False)
+
+        label_widget.grid( column=0 , padx=5, pady=5)
+        table.grid(column= 1,row=0)
         vid = cv2.VideoCapture(0)
 
         if not vid.isOpened():
@@ -63,6 +81,14 @@ class FaceRecognition:
                     dt_string = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
                     self.data[name] = {}
                     self.data[name]["last attendence time"] = dt_string
+                    print(self.data)
+                    '''if name != "unknown":
+                        panelC = tk.Label(image=cv2.imread(f"./my_db/{name}"))'''
+                    table.insert(parent='', text="key", index=tk.END, values=(self.data[name]['name'], dt_string))
+                    table.grid(column= 1,row=0)
+                    table.configure()
+                    table.update()
+                    root.update()
 
                 with concurrent.futures.ThreadPoolExecutor() as executor :
                     executor.map(match_faces,self.face_encodings)
@@ -79,10 +105,16 @@ class FaceRecognition:
                 cv2.rectangle(frame, (l,b-35),(r,b),(0,0,255),-1)
                 cv2.putText(frame,name, (l+6,b-6),cv2.FONT_HERSHEY_DUPLEX,0.8,(255,255,255),1)
 
-            cv2.imshow('FAce Recognition',frame)
+            #cv2.imshow('FAce Recognition',frame)
+            opencv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+            captured_image = Image.fromarray(opencv_image)
+            photo_image = ImageTk.PhotoImage(image=captured_image)
+            label_widget.configure(image=photo_image)
+
+            root.update()
             if cv2.waitKey(1) == ord('q'):
                 break
-
+        root.mainloop()
         vid.release()
         cv2.destroyAllWindows()
 
@@ -91,8 +123,3 @@ if __name__ == '__main__':
     fr = FaceRecognition()
     fr.run_recognition()
     print(fr.data)
-
-
-if __name__ == '__main__':
-    fr = FaceRecognition()
-    fr.run_recognition()
